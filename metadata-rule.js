ea729c63d1e2859e0eb1564cd4b9376940ade993
Sandeep Neerarambham <sandeep.neerarambham@gmail.com>
2018-10-05 13:40:16 +1000

    Sandeepneerarambham patch 1 (#17)
    * save access_token to global object and restructure
    
    * use password realm grant instead of default directory
    
    parse the user object.
    
    * make the migration namespace claim client specific
    
    * update get_user script to use auth0 extension tools
    
    * Update README.md
    
    * remove getUser
    
    * Update metadata-rule.js
    
    * Update metadata-rule.js
    
    * Update get_user.js

--- metadata-rule.js
+++ metadata-rule.js
@@ -2,6 +2,15 @@ function auth0migrateUser(user, context, callback) {
     if (context.clientID === "{CLIENT_ID}") {
         context.idToken['https://migrationapp/user_metadata'] = user.user_metadata || {};
         context.idToken['https://migrationapp/app_metadata'] = user.app_metadata || {};
-      }
-    callback(null, user, context);
+        user.app_metadata = user.app_metadata || {};
+        user.app_metadata.migration_complete = true;
+        auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
+          .then(function(){
+          callback(null, user, context);
+         })
+         .catch(function(err){
+          callback(err);
+         });
+       }
+     callback(null, user, context);
 }
