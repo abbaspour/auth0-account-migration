0e0a1a5c03914b8b81166ab5e3123fc87fd09d8b
Sandeep Neerarambham <sandeep.neerarambham@gmail.com>
2018-10-05 13:21:38 +1000

    Update metadata-rule.js

--- metadata-rule.js
+++ metadata-rule.js
@@ -1,7 +1,16 @@
 function auth0migrateUser(user, context, callback) {
-    if (context.clientID === "{CLIENT_ID}") {
+    if (context.clientID === "4no0uSWSQDLUeSZxsxoXWctD6cKHiryl") {
         context.idToken['https://migrationapp/user_metadata'] = user.user_metadata || {};
         context.idToken['https://migrationapp/app_metadata'] = user.app_metadata || {};
-      }
-    callback(null, user, context);
+        user.app_metadata = user.app_metadata || {};
+        user.app_metadata.migration_complete = true;
+        auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
+          .then(function(){
+          callback(null, user, context);
+         })
+         .catch(function(err){
+          callback(err);
+         });
+       }
+     callback(null, user, context);
 }
